9dd285cd322b1c9d2b893bd1190d78f1566102d5
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-12-04 11:39:08 -0500

    added firebase to project

---
+++ src/firebase.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; // Import for Realtime Database
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAI737PvEjkSRu0S267zANxBrqMErNLEvU",
+  authDomain: "browser-insights-d704b.firebaseapp.com",
+  databaseURL: "https://browser-insights-d704b-default-rtdb.firebaseio.com",
+  projectId: "browser-insights-d704b",
+  storageBucket: "browser-insights-d704b.firebasestorage.app",
+  messagingSenderId: "1061503587334",
+  appId: "1:1061503587334:web:cc4fb1835ccffffccd1f87",
+  measurementId: "G-4QX7RV91TK"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the Realtime Database service
+export const db = getDatabase(firebaseApp);
