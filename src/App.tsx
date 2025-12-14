8873a273f2c57c694ac571878314e102625f5968
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-12-13 21:19:56 -0500

    DB is used

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState, type ReactElement } from 'react'
 import './App.css'
+import { db } from './firebase'
+import { ref, child, get, type DatabaseReference } from "firebase/database";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<DatabaseReference>({} as DatabaseReference);
+  const [dev1, setDev1] = useState<DatabaseReference>({} as DatabaseReference);
+  const [dev2, setDev2] = useState<DatabaseReference>({} as DatabaseReference);
+
+  useEffect(() => {
+    const path = `users/1`;
+
+    const nodeRef = ref(db, path);
+    get(nodeRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setUser(snapshot.val());
+        } else {
+          console.debug('No data available at', path);
+        }
+      })
+      .catch((error) => {
+        console.error('Realtime DB get() error:', error);
+      });
+
+    get(ref(db, `users/1/user_devices/1`)).then((snapshot) => {
+      console.debug('Device 1 data:', snapshot.val());
+      setDev1( snapshot.val() );
+    } );
+
+    get(ref(db, `users/1/user_devices/2`)).then((snapshot) => {
+      console.debug('Device 2 data:', snapshot.val());
+      setDev2( snapshot.val() );
+    } );
+  }, []);
+
+
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Browser History</h2>
+        <p>Welcome {user.user_primary_email}</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h3>Devices:</h3>
+        <ul>
+          <li> {dev1.device_name} Pairing Code: {dev1.device_pairing_code}</li>
+          <li> {dev2.device_name} Pairing Code: {dev2.device_pairing_code}</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </> 
   )
 }
 
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"; // Import for Realtime Database
+import { getDatabase, Database } from "firebase/database";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app =  initializeApp(firebaseConfig);
 
 // Get a reference to the Realtime Database service
-export const db = getDatabase(firebaseApp);
+export const db : Database = getDatabase(app);
