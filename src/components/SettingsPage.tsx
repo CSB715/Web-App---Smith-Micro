9a0e94aab3ea1183237b901f3e36664e70ae8ef0
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2026-01-15 18:48:53 -0500

    links added to settings

--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -1,11 +1,36 @@
 // Settings Page Component
 // STUB
 
+import { Link } from "react-router";
+
 function Settings() {
+
+
+    // Page Name
+    // -------------
+    // Flagged Sites
+    // -------------
+    // Site Categories
+    // -------------
+    // Notifications
+    // -------------
+    // Account
+    // -------------
+    // 
+    // Nav Bar
+
+
     return (
         <>
-            <h1>Settings Page - Stub</h1>
-            {/* Settings Page Content */}
+            <h1>Settings Page</h1>
+            <hr></hr>
+            <Link to="/settings/flagged-sites">Flagged Sites</Link>
+            <hr></hr>
+            <Link to="/settings/site-categories">Site Categories</Link>
+            <hr></hr>
+            <Link to="/settings/notifications">Notifications</Link>
+            <hr></hr>
+            <Link to="/settings/account">Account</Link>
         </>
     )
 }
