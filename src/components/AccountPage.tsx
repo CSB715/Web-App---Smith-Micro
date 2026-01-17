628d7544fdde600eb89df2a737d3d7c51d335939
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2026-01-16 20:13:01 -0500

    added account page authentication

--- src/components/AccountPage.tsx
+++ src/components/AccountPage.tsx
@@ -1,10 +1,33 @@
 // Account Page Component
-// STUB
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import type { User } from "firebase/auth";
+import { useNavigate } from "react-router";
+import "./firestore";
 
 function Account() {
+    const [user, setUser] = useState<User | null | undefined>(undefined); // undefined = checking
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const auth = getAuth();
+        const unsub = onAuthStateChanged(auth, (u) => {
+        if (u) {
+            setUser(u);
+        } else {
+            setUser(null);
+            navigate("/login", { replace: true });
+        }
+        });
+
+        return () => unsub();
+    }, [navigate]);
+
+    if (user === undefined) return null; // or a loader
+
     return (
         <>
-            <h1>Account Page - Stub</h1>
+            <h1>Account</h1>
             {/* Account Page Content */}
         </>
     )
