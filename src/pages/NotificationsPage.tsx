63e7ae316b0dd8d8ea6cc64ac95601a91c530b5c
Seth Harmon <harmonsc21@gcc.edu>
2026-01-20 21:20:39 -0500

    implementing site modal

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -2,7 +2,26 @@
 // STUB
 
 import { useEffect, useState } from "react";
-import { GetDocs } from "../utils/firestore";
+import { GetDoc, GetDocs } from "../utils/firestore";
+import SiteModal from "../components/SiteModal";
+
+function getTimeDifferenceString(date: Date): string {
+  const now = new Date();
+  const diffMs = now.getTime() - date.getTime();
+  if (diffMs > 1000 * 60 * 60 * 24) {
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    return `${diffDays} day${diffDays !== 1 ? "s" : ""}`;
+  }
+  if (diffMs > 1000 * 60 * 60) {
+    const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+    return `${diffHours} hour${diffHours !== 1 ? "s" : ""}`;
+  }
+  if (diffMs > 1000 * 60) {
+    const diffMinutes = Math.floor(diffMs / (1000 * 60));
+    return `${diffMinutes} minute${diffMinutes !== 1 ? "s" : ""}`;
+  }
+  return `${Math.floor(diffMs / 1000)} second${Math.floor(diffMs / 1000) !== 1 ? "s" : ""}`;
+}
 
 function Notifications() {
   const [notifications, setNotifications] = useState<any[]>([]);
@@ -19,12 +38,14 @@ function Notifications() {
   return (
     <>
       <h1>Notification History</h1>
-      {/* Notifications Page Content */}
       <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
         {notifications.map((notification, index) => (
           <li key={index}>
-            <p>{notification.siteURL}</p>
-            <p>{notification.dateTime.toDate().toDateString()}</p>
+            <SiteModal
+              url={notification.siteURL}
+              user_id="7LpcmhJK1QCWn9ETqLN5"
+            />
+            <p>{getTimeDifferenceString(notification.dateTime.toDate())} ago</p>
             <p>{notification.reason}</p>
           </li>
         ))}
