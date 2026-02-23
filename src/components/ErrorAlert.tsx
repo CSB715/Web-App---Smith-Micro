5dc7d3e2f463f8adf3ef8d74903a66304683a867
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2026-02-23 13:33:05 -0500

    Revert "Updates"
    This reverts commit 46812e11e5c99a4842dcb76a673844e39e8c88ec.

---
+++ src/components/ErrorAlert.tsx
@@ -0,0 +1,30 @@
+import "../styles/Alert.css";
+import { useRef } from "react";
+
+function closeAlert() {
+    const modal = document.getElementById("errorAlert");
+    modal!.style.display = "none";
+}
+
+function showErrorModal() {
+    const modal = document.getElementById("errorAlert");
+    modal!.style.display = "block";
+}
+
+function ErrorAlert() {
+    const overlayRef = useRef<HTMLDivElement | null>(null);
+    
+    return (
+        <div id="errorAlert" className="alert"
+        ref={overlayRef}
+        onClick={(e) => {if (e.target === overlayRef.current) closeAlert(); }}> 
+            <div className="alert-content">
+                <span className="close" onClick={() => closeAlert()}>&times;</span>
+                <p>We're sorry, an error occurred. Please try again later.</p>
+            </div>
+        </div> 
+    );
+}
+
+export default ErrorAlert;
+export { showErrorModal };
