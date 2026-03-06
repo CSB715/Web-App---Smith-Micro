ce12c330216220aeb16d72c30776b6747429bee4
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2026-03-06 15:39:09 -0500

    device complete deletion

--- src/components/DeleteDeviceModal.tsx
+++ src/components/DeleteDeviceModal.tsx
@@ -1,57 +1,53 @@
 import type { DocumentData } from "firebase/firestore";
 import { useRef } from "react";
 import { showErrorModal } from "./ErrorAlert";
-import { doc, deleteDoc } from "firebase/firestore";
-import { getDb, getAuthInstance, GetUserDevices, DeleteCollection } from "../utils/firestore";
+import { doc } from "firebase/firestore";
+import { getDb, getAuthInstance, GetUserDevices, DeleteDevice } from "../utils/firestore";
 import "../styles/Modal.css";
 
 
 type Props = {
-    currDevice : DocumentData | null,
-    updateDevices : (data: Array<DocumentData>) => void
+  currDevice : DocumentData | null,
+  updateDevices : (data: Array<DocumentData>) => void
 }
 
 function closeModal() {
-    const modal = document.getElementById("deleteDeviceModal");
-    modal!.style.display = "none";
+  const modal = document.getElementById("deleteDeviceModal");
+  modal!.style.display = "none";
 }
 
-function deleteDevice(currDevice : DocumentData, updateDevices : (data: Array<DocumentData>) => void) {
-    const docRef = doc(getDb(), "Users", getAuthInstance().currentUser!.uid, "Devices", currDevice.id);
+async function deleteDevice(currDevice : DocumentData, updateDevices : (data: Array<DocumentData>) => void) {
 
-    DeleteCollection(docRef.path + "/Visits").then(() => {
-        deleteDoc(docRef)
-        .then(async () => {
-            closeModal()
-            GetUserDevices(doc(getDb(), "Users", getAuthInstance().currentUser!.uid)).then((docArr) => {
-                updateDevices(docArr)
-            });
-        })
-        .catch((error) => {
-            console.error("Error removing device: ", error);
-            closeModal()
-            showErrorModal();                // show error modal
-        });
+  DeleteDevice(currDevice).then(async () => {
+    closeModal()
+    GetUserDevices(doc(getDb(), "Users", getAuthInstance().currentUser!.uid)).then((docArr) => {
+      updateDevices(docArr);
     });
+  })
+  .catch((error) => {
+    console.error("Error removing device: ", error);
+    closeModal()
+    showErrorModal();                // show error modal
+  });
 }
 
 export default function DeleteDeviceModal({ currDevice, updateDevices } : Props) {
-    const overlayRef = useRef<HTMLDivElement | null>(null);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
 
-    return (
-        <div id="deleteDeviceModal" className="modal"
-        ref={overlayRef}
-        onClick={(e) => {if (e.target === overlayRef.current) closeModal()}}> 
-            <div className="modal-content">
-                <span className="close" onClick={() => closeModal()}>&times;</span>
-                <p>Delete {currDevice?.name}?</p>
-                <p>If you delete this device, all data associated with it will be lost.</p>
-                <div>
-                    <button id="cancelDeleteDevice" onClick={() => closeModal()}>Cancel</button>
-                    <button id="confirmDeleteDevice" onClick={() => deleteDevice(currDevice!, updateDevices)}>Confirm</button>
-                </div>
-            </div>
+  return (
+    <div id="deleteDeviceModal" className="modal"
+    ref={overlayRef}
+    onClick={(e) => {if (e.target === overlayRef.current) closeModal()}}> 
+      <div className="modal-content">
+        <span className="close" onClick={() => closeModal()}>&times;</span>
+        <p>Delete {currDevice?.name}?</p>
+        <p>If you delete this device, all data associated with it will be lost.</p>
+        <div>
+          <button id="cancelDeleteDevice" onClick={() => closeModal()}>Cancel</button>
+          <button id="confirmDeleteDevice" onClick={() => deleteDevice(currDevice!, updateDevices)}>Confirm</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 
 }
